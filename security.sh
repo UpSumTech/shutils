41813e6e3d6b6064ebb29bcbaffcc4643dadf4e5
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-11-23 22:53:16 -0800

    Some security related commands to get a better idea of who is logged in and when the system rebooted etc

--- security.sh
+++ security.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env bash
 
 uptime # How long has this machine been running
+who # who is currently logged into the system
+w # List processes belonging to logged in users
+
+last # List last logged in users
+last reboot # List last time the system restarted
+last root # List last time the system was accessed as root
+
+wall 'Taking down system for maintenance' # Send a warning to other users to wrap up their work before maintenance window begins
 lsb_release -a # List distro info
 lsmod # Lsit all the kernel modules
 dpkg -l # List all packages
