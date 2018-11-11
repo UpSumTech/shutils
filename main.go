378dfbf1caca478d8d23de62adec6f454f307308
Suman Mukherjee <sumanmukherjee03@gmail.com>
2018-11-11 15:13:10 -0800

    Starting a go project

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootShortDesc = "Shutils is a simple utilty to print use cases of various sysadmin CLI tools"
+	rootLongDesc  = `Shutils is a flexible tool built in golang.
+	It prints cheat sheets for various sysadmin CLI tools.`
+	cfgFile string
+	Dryrun  bool
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:              "shutils [sub]",
+		Short:            rootShortDesc,
+		Long:             rootLongDesc,
+		TraverseChildren: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("example usage")
+		},
+	}
+
+	rootCmd.Execute()
+}
