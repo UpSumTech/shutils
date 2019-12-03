d32894fdf47f1179fc178be2ed8cfa5c9a363adc
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-12-03 14:32:40 -0800

    Added a kubectl debug command

--- cmd/kubecmds/kubectl_run_cmds.go
+++ cmd/kubecmds/kubectl_run_cmds.go
@@ -22,6 +22,9 @@ kubectl run -i -t --rm test-pod --image=ubuntu:16.04 --restart=Never --serviceac
 
 # kubectl run arbitrary pod and execute some task quickly
 kubectl run test-pod --record --wait=true --image-pull-policy=Always --image=ubuntu:16.04 --restart=Never --env="DB_HOST=mysql.example.com" --env="DB_PORT=3306" --command -- /bin/bash -c 'while true; do sleep 3; done'; sleep 10; kubectl get pod test-pod; kubectl cp $HOME/.secrets test-pod:/root/.secrets; kubectl cp $(pwd) test-pod:/root/example-repo; kubectl exec test-pod -- /bin/bash -c "cd /root/example-repo; ./bin/run.sh"; kubectl delete pod test-pod
+
+# If you want to annotate a pod with a specific iam role that can be assumed with kube2iam
+kubectl annotate pods test-pod "iam.amazonaws.com/role"="arn:aws:iam::<accountid>:role/<some_iam_role>"
 			`)
 		},
 	}
