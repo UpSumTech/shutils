85228aad24822e08ff6399ebe32fe036bc2a02b7
Suman Mukherjee <sumanmukherjee03@gmail.com>
2020-06-24 12:51:45 -0700

    Some tiny updates to get users in a unix system

--- cmd/usercmds/init.go
+++ cmd/usercmds/init.go
@@ -23,16 +23,28 @@ func Init() *cobra.Command {
 		TraverseChildren: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println(`
-# Scenario : lets say you added a temp user in a m/c to test stuff out and then want to cleanly remove it
 useradd -m -G developer -g developer --s /bin/bash -c developer,,,, developer # This adds a user with a shell
+
 cat /etc/group | grep sudo || groupadd sudo # Add the sudo group if it already isnt there
+
 usermod -a -G sudo developer # Modify the user and add him to the sudo group
+
 deluser developer --remove-all-files # delete the user with all his files
+
 ps U developer # Kill all processes belonging to a deleted user
+
 slay -clean developer # Kills all remaining processes from the above step
+
 groupdel sudo # To delete a group
 
 echo 'developer ALL=(ALL) NOPASSWD:/usr/bin/top' >> /etc/sudoers # To only allow a user to execute a single command
+
+awk -F':' '{ print $1}' /etc/passwd # To list all users in a system
+getent passwd # To list all users in /etc/passwd
+
+# To get all login users
+uid_min=$(grep "^UID_MIN" /etc/login.defs)
+awk -F':' -v "limit=${uid_min##UID_MIN}" '{ if ( $3 >= limit ) print $1}' /etc/passwd
 			`)
 		},
 	}
