c430d6ad9cb0f1826c60ea5c9a8e930f71814c14
Suman Mukherjee <sumanmukherjee03@gmail.com>
2020-08-20 12:24:40 -0700

    Added some commands for mounting ebs volumes

--- cmd/diskcmds/init.go
+++ cmd/diskcmds/init.go
@@ -35,6 +35,15 @@ cat /etc/fstab # Get the static file system info
 free -m # Get free memory statistics for the system
 cat /etc/fstab # Get the information on mounted volumes
 file -s /dev/xvda # Check whether the mounted device has any data or not
+
+# Process to obtain device name where ebs volume is attached
+vol_id="<ebs_volume_id>"
+vol_id_on_disk="$(echo "$vol_id" | tr -d '-')"
+device_id="$(ls /dev/disk/by-id/*-Amazon_Elastic_Block_Store_$vol_id_on_disk | head -1)"
+device_name="/dev/$(readlink "$device_id" | tr / '\n' | tail -1)"
+
+blkid -o value -s TYPE $device_name || mkfs -t ext4 "$device_name" # Check if volume is already formatted or not. If not format it to ext4
+mkdir -p "<dir_you_want_to_mount_volume_to>" && mount "$dev_name" "<dir_you_want_to_mount_volume_to>" # mount ebs volume in a ec2 machine
 			`)
 		},
 	}
